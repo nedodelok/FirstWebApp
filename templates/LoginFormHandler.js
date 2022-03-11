c7a76fb6d7545e5f2b11052577065061f765a279
Артём Носов <ortemkek@gmail.com>
2022-03-12 01:45:49 +0300

    ПУТИН ВЕРНИ КРСЫВМ

--- templates/LoginFormHandler.js
+++ templates/LoginFormHandler.js
@@ -1,33 +1,39 @@
+function ValidationName() {
+    var reg = /^([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$/
+    var myMail = form.querySelector('.Name').value;
+    var valid = reg.test(myMail);
+    if (valid) ;
+    else  {
+        var error = form.querySelector('.Name')
+    error.className='error'
+    error.style.color = 'red'
+    };
+
+}
+
+function ValidationSurname() {
+    var reg = /^([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$/
+    var myMail = form.querySelector('.Surname').value;
+    var valid = reg.test(myMail);
+    if (valid);
+else  {
+        var error = form.querySelector('.Surname')
+    error.className='error'
+    error.style.color = 'red'
+    };
+}
 
 let form = document.querySelector('.LoginForm')
 form.addEventListener('submit', function (event) {
-  let name = form.elements.Name
-  let surname = form.elements.Surname
-  let age = form.elements.Age
-  let password = form.elements.Password
-  let email = form.elements.Email
-  event.preventDefault()
-var fields = form.querySelectorAll('.field')
-
-form.addEventListener('submit', function (event) {
-  event.preventDefault()
-
-    var errors = form.querySelectorAll('.error')
-
-  for (var i = 0; i < errors.length; i++) {
-    errors[i].remove()
-  }
-
-  for (var i = 0; i < fields.length; i++) {
-    if (!fields[i].value) {
-      console.log('field is blank', fields[i])
-      var error = document.createElement('div')
-    error.className='error'
-    error.style.color = 'red'
-    error.innerHTML = 'соси хуй'
-    form[i].parentElement.insertBefore(error, fields[i])
-    }
-  }
-})
+    event.preventDefault()
+    let login = form.querySelector('.Login')
+  let name = form.querySelector('.Name')
+  let surname = form.querySelector('.Surname')
+  let age = form.querySelector('.Age')
+  let password = form.querySelector('.Password')
+  let email = form.querySelector('.Email')
+      console.log('name: ', name.value)
+    ValidationName()
+    ValidationSurname()
 
 })
