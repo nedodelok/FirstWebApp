e39bee579afdb053e13a058ced7df3b018a1e9fe
Никита Вишневский <nikitavish@list.ru>
2022-03-12 11:44:54 +0300

    0:24

--- templates/LoginFormHandler.js
+++ templates/LoginFormHandler.js
@@ -5,9 +5,10 @@ function ValidationName() {
     var valid = reg.test(myMail);
     if (valid) ;
     else  {
-        var error = form.querySelector('.Name')
-    error.className='error'
-    error.style.color = 'red'
+    let div = document.createElement('div');
+    div.className = "alert";
+    div.innerHTML = "Недопустимое имя";
+    nameid.after(div)
     };
 
 }
@@ -19,32 +20,45 @@ function ValidationSurname() {
     if (valid);
 else  {
         var error = form.querySelector('.Surname')
-    error.className='error'
-    error.style.color = 'red'
+    let div = document.createElement('div');
+    div.className = "alert";
+    div.innerHTML = "Недопустимая фамилия";
+    surnameid.after(div)
     };
 }
 
 function AroundAge() {
     var vozrast  = form.querySelector('.Age').value;
     if (vozrast > 120) {
-        alert("Число должно быть меньше или равно 120")
+    let div = document.createElement('div');
+    div.className = "alert";
+    div.innerHTML = "Число должно быть меньше или равно 120";
+    ageid.after(div)
     }
     if (vozrast < 1) {
-        alert("Число должно быть больше или равно 1")
+    let div = document.createElement('div');
+    div.className = "alert";
+    div.innerHTML = "Число должно быть больше или равно 1";
+    ageid.after(div)
     }
 }
 
 let form = document.querySelector('.LoginForm')
 form.addEventListener('submit', function (event) {
     event.preventDefault()
-    let login = form.querySelector('.Login')
-  let name = form.querySelector('.Name')
-  let surname = form.querySelector('.Surname')
-  let age = form.querySelector('.Age')
-  let password = form.querySelector('.Password')
-  let email = form.querySelector('.Email')
-      console.log('name: ', name.value)
+  var alerts = form.querySelectorAll('.alert')
+
+  for (var i = 0; i < alerts.length; i++) {
+    alerts[i].remove()
+  }
     ValidationName()
     ValidationSurname()
     AroundAge()
+  let email = form.querySelector('.Email')
+    if (!email.value){
+    let div = document.createElement('div');
+    div.className = "alert";
+    div.innerHTML = "Заполните поле";
+    emailid.after(div)
+    }
 })
